f798e7e705779c1320e8f16073bef07de2f0fd1b
jacribeiro <j.alberto.cribeiro@gmail.com>
2024-01-31 22:29:48 +0000

    FilterBox now has Accordion elements to display and hide professors

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -1,5 +1,70 @@
 import PropTypes from "prop-types";
 import { useState, useEffect, useCallback } from "react";
+import { styled } from "@mui/material/styles";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+
+const subareas_professors = {
+  "Arquiteturas e Sistemas de Computação": {
+    primary: [],
+    secondary: [],
+  },
+  "Computação Gráfica e Media Digitais Interativos": {
+    primary: [],
+    secondary: [],
+  },
+  "Engenharia de Software": {
+    primary: [],
+    secondary: [],
+  },
+  "Ciência e Tecnologia da Programação": {
+    primary: [],
+    secondary: [],
+  },
+  "Sistemas de Informação": {
+    primary: [],
+    secondary: [],
+  },
+  "Sistemas Inteligentes": {
+    primary: [],
+    secondary: [],
+  },
+};
+
+// Processamento dos professores e subáreas
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(() => ({
+  "&::before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowRightIcon sx={{ fontSize: "1.3rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  flexDirection: "row-reverse",
+  display: "flex",
+  alignItems: "center",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    margin: 0,
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(1),
+}));
 
 const FilterBox = ({ onCheckedChange, className }) => {
   const [checkedState, setCheckedState] = useState({
@@ -12,6 +77,33 @@ const FilterBox = ({ onCheckedChange, className }) => {
     multiple: true,
     noSubarea: false,
   });
+
+  useEffect(() => {
+    for (const subarea in subareas_professors) {
+      subareas_professors[subarea].primary = [];
+      subareas_professors[subarea].secondary = [];
+    }
+
+    fetch("/subareas_professors.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const professors = data.professors;
+        for (const professor of professors) {
+          if (Object.keys(subareas_professors).includes(professor.primary)) {
+            subareas_professors[professor.primary].primary.push(professor.name);
+          }
+          if (Object.keys(subareas_professors).includes(professor.secondary)) {
+            subareas_professors[professor.secondary].secondary.push(
+              professor.name
+            );
+          }
+        }
+        console.log(subareas_professors);
+      })
+      .catch((error) =>
+        console.error("Could not read professors file:", error)
+      );
+  }, []);
 
   useEffect(() => {
     onCheckedChange(makeCheckedObject(checkedState));
@@ -80,8 +172,8 @@ const FilterBox = ({ onCheckedChange, className }) => {
   return (
     <div id="legend" className={className}>
       <h2>Legenda</h2>
-      <ul>
-        <li>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#498b49" }}></span>
           <div className="checkbox-item">
             <label htmlFor="subarea1">
@@ -94,8 +186,28 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ul>
+            <li>Primary</li>
+            <li>
+              <ul>
+                {subareas_professors[
+                  "Arquiteturas e Sistemas de Computação"
+                ].primary.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </li>
+            <li>Secondary</li>
+            <li>
+              <ul></ul>
+            </li>
+          </ul>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#f2be22" }}></span>
           <div className="checkbox-item">
             <label htmlFor="subarea2">
@@ -108,8 +220,11 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#ff7b2f" }}></span>
           <div className="checkbox-item">
             <label htmlFor="subarea3">Engenharia de Software</label>
@@ -120,8 +235,11 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#fc8bba" }}></span>
           <div className="checkbox-item">
             <label htmlFor="subarea4">
@@ -134,8 +252,11 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#146c94" }}></span>
           <div className="checkbox-item">
             <label htmlFor="subarea5">Sistemas de informação</label>
@@ -146,8 +267,11 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#dd98e4" }}></span>
           <div className="checkbox-item">
             <label htmlFor="subarea6">Sistemas inteligentes</label>
@@ -158,8 +282,11 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#6ab0ff" }}></span>
           <div className="checkbox-item">
             <label htmlFor="multiple">Múltiplas sub-áreas</label>
@@ -170,8 +297,11 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-        <li>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
           <span className="color" style={{ backgroundColor: "#444" }}></span>
           <div className="checkbox-item">
             <label htmlFor="noSubarea">Sem sub-áreas</label>
@@ -182,8 +312,9 @@ const FilterBox = ({ onCheckedChange, className }) => {
               onChange={handleInputChange}
             />
           </div>
-        </li>
-      </ul>
+        </AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
       <span id="legend-reset" className="reset" onClick={resetCheckboxes}>
         Selecionar todos
       </span>
